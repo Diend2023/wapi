e2e38025517716fee4deea01ea7ab1e9e3377583
Diend2023 <2577569250@qq.com>
2025-06-16 20:01:11 +0800

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     items_per_page: int = 50
-    host: str = "127.0.0.1"
+    host: str = "::"
     port: int = 2577
 
 settings = Settings()
